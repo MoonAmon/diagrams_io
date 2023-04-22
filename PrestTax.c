a9c977bd8e08c47033531f96aeec89fe64bcff26
MoonThi <misterofclans.mo@gmail.com>
2023-04-22 19:19:09 -0300

    first commit

---
+++ PrestTax.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdint.h>
+
+int main(int argc, char const *argv[])
+{
+	int num;
+
+	printf("Digite o nuemro:%s\n");
+	scanf("%d", &num);
+	int numX = num * num
+
+	printf("Resultado:%d",numX); 
+
+	return 0;
+}
